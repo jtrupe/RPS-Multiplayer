c6bb7539c15acb144b33085eec32908ba9e84591
Julian Trupe <juliantrupe@gmail.com>
2019-11-02 15:18:48 -0700

    minor changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,10 +16,15 @@ var database = firebase.database();
 var P1wins = 0;
 var P2wins = 0;
 var ties = 0;
-var P1name = "" ;
-var P2name = "" ;
+var P1name = "";
+var P2name = "";
+var P1turnTaken = false;
+var P2turnTaken = false;
+var gameStart = false;
 
-
+if ((P1name != "") && (P2name != "")) {
+    gameStart = true;
+}
 
 // Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
@@ -29,18 +34,18 @@ var P1winsText = document.getElementById("P1wins-text");
 var P2winsText = document.getElementById("P2wins-text");
 var tiesText = document.getElementById("ties-text");
 
-$(document).on("click" , "#addP1" , function(){
+$(document).on("click", "#addP1", function () {
     event.preventDefault();
-    P1name = $("#player1input").val() ;
+    P1name = $("#player1input").val();
     $("#player1input").remove();
     $("#addP1").remove();
     $("#P1label").remove();
     console.log(P1name);
 })
 
-$(document).on("click" , "#addP2" , function(){
+$(document).on("click", "#addP2", function () {
     event.preventDefault();
-    P2name = $("#player2input").val() ;
+    P2name = $("#player2input").val();
     $("#player2input").remove();
     $("#addP2").remove();
     $("#P2label").remove();
@@ -48,14 +53,18 @@ $(document).on("click" , "#addP2" , function(){
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
-    if(P1guess){
-        var P2guess = event.key ;
+    console.log(gameStart);
+    if (gameStart) {
+        if (!P1turnTaken) {
+            var P1guess = event.key;
+            P1turnTaken = true;
+        }
+        else if (!P2turnTaken)
+            var P2guess = event.key;
+            P2turnTaken = true ;
     }
-    else {
-        var P1guess = event.key;
-    }
-
-    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+}
+if (gameStart && P1turnTaken && P2turnTaken) { 
     if ((P1guess === "r") || (P1guess === "p") || (P1guess === "s")) {
 
         if ((P1guess === "r" && P2guess === "s") ||
@@ -78,4 +87,5 @@ document.onkeyup = function (event) {
         P2winsText.textContent = "losses: " + P2wins;
         tiesText.textContent = "ties: " + ties;
     }
+
 };
